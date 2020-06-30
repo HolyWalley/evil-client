1c378dd98fa92455edbe79a167aac824e02378c4
HolyWalley <yasha.krasnov@gmail.com>
2020-06-30 12:49:55 +0300

    add vdn support

--- lib/evil/client/formatter.rb
+++ lib/evil/client/formatter.rb
@@ -21,6 +21,7 @@ class Evil::Client
       return to_yaml(source) if format == :yaml
       return to_form(source) if format == :form
       return to_text(source) if format == :text
+      return to_json(source) if format == :vdn
 
       to_multipart(source, opts)
     end
